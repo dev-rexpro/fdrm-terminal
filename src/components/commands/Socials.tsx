e0c3428e35f89a5b0266a3c0f64d0d2183b35a9e
REXPro <dev.rexpro@gmail.com>
2026-07-03 09:47:50 +0000

    1.0.4

--- src/components/commands/Socials.tsx
+++ src/components/commands/Socials.tsx
@@ -52,25 +52,25 @@ const socials = [
   {
     id: 1,
     title: "GitHub",
-    url: "https://github.com/satnaing",
+    url: "https://github.com/fdrm",
     tab: 3,
   },
   {
     id: 2,
     title: "Dev.to",
-    url: "https://dev.to/satnaing",
+    url: "https://dev.to/fdrm",
     tab: 3,
   },
   {
     id: 3,
     title: "Facebook",
-    url: "https://www.facebook.com/satnaing.dev",
+    url: "https://www.facebook.com/dev.rexpro",
     tab: 1,
   },
   {
     id: 4,
     title: "Instagram",
-    url: "https://instagram.com/satnaing.dev",
+    url: "https://instagram.com/dev.rexpro",
     tab: 0,
   },
 ];
